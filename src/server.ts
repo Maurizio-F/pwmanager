0ccb168ea183e6917693f0f68e9237b6d3ec1efd
Maurizio-F <end2zone@googlemail.com>
2021-05-11 15:19:03 +0200

    :heavy_plus_sign: Add inquirer

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,15 @@
-import fs from "fs";
+import { askForMainPassword } from "./utils/questions";
+import { isMainPasswordValid } from "./utils/validation";
 
-console.log(fs.readFileSync("./README.md", "utf-8"));
+// function start() {
+const start = async () => {
+  /* Solution with while */
+  let mainPassword = await askForMainPassword();
+  while (!isMainPasswordValid(mainPassword)) {
+    console.log("Is invalid");
+    mainPassword = await askForMainPassword();
+  }
+  console.log("Is valid");
+};
+
+start();
