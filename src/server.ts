5aad0295ecc76d0d96a04a7161695397317069b7
Maurizio-F <end2zone@googlemail.com>
2021-05-19 13:44:33 +0200

    :sparkles: Connect to database

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,31 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import express from "express";
+import { connectDatabase } from "./utils/database";
+
+if (process.env.MONGO_URL === undefined) {
+  throw new Error("Missing env Mongo_URL");
+}
 
 const app = express();
 const port = 5000;
 
 app.get("/api/credentials", (_request, response) => {
-  response.send("All credentials request");
+  response.json("All credentials request");
 });
 
 app.post("/api/credentials", (_request, response) => {
   response.send("Add new credential");
 });
 
-app.listen(port, () => {
-  console.log("pwmanager listening at http://localhost:${port}");
+connectDatabase(process.env.MONGO_URL).then(() => {
+  console.log("Database connected");
+  app.listen(port, () => {
+    console.log("pwmanager listening at http://localhost:${port}");
+  });
 });
 
 app.delete("/api/credentials", (_request, response) => {
-  response.send("All credentials request");
+  response.send("Delete");
 });
