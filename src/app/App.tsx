cd51583981295bf206aad0486da40064db8396f9
Maurizio-F <end2zone@googlemail.com>
2021-05-21 10:45:54 +0200

    :fire: App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,7 @@
-import React, { useState } from "react";
-import logo from "./logo.svg";
+import React from "react";
 import styles from "./App.module.css";
 
 function App(): JSX.Element {
-  const [count, setCount] = useState<number>(0);
-
   return (
     <div className={styles.App}>
       <h1>pwmanager</h1>
